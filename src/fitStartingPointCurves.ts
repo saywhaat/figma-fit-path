09c93e5d280cc12c4f6bbd44de1034ff75ef5abe
Dmitrii Kuznetcov <kuznecov.da@medindex.ru>
2021-12-17 15:18:38 +0300

    11

--- src/fitStartingPointCurves.ts
+++ src/fitStartingPointCurves.ts
@@ -4,4 +4,17 @@ export default function fitStartingPointCurves(
   x0: Point,
   x1: Point,
   x2: Point
-) {}
+): [Point, Point] {
+  const v0 = [x2[0] - x1[0], x2[1] - x1[1]];
+  const v1 = [x1[0] - x0[0], x1[1] - x0[1]];
+  const v2 = [x2[0] - x0[0], x2[1] - x0[1]];
+
+  const l0 = v0[0] * v0[0] + v0[1] * v0[1];
+  const a1 = v0[0] * v1[0] + v0[1] * v1[1];
+  const a2 = v0[0] * v2[0] + v0[1] * v2[1];
+
+  return [
+    [x0[0] + (v0[0] * a1) / l0, x0[1] + (v0[1] * a1) / l0],
+    [x0[0] + (v0[0] * a2) / l0, x0[1] + (v0[1] * a2) / l0],
+  ];
+}
--- src/plugin.test.js
+++ src/plugin.test.js
@@ -2,7 +2,7 @@ const fitStartingPointCurves = require("./fitStartingPointCurves").default;
 
 test("!", () => {
   const fitted = fitStartingPointCurves([13, 8], [2, 6], [12, 1]);
-  expect(fitted).toBe([
+  expect(fitted).toStrictEqual([
     [5, 12],
     [15, 7],
   ]);
